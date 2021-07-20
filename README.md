e035aa50a402fe1a4f2b9acbacf5681e42cfae7c
flpcan <felipecandia@live.com>
2021-07-20 17:44:01 -0400

    works with steam

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # steam_crawler
 
-funciona para techwithtim
-solo necesito configurarlo para steam estare buscando por clases
-todo bien !
+Actualizado
+Funciona para steam
+Necesito aplicar para excel, archivo de texto, csv, postgresql?
--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
+import time
 
-PATH = "/usr/local/bin/chromedriver"
-driver = webdriver.Chrome(PATH)
 
-driver.get("https://techwithtim.net")
+driver = webdriver.Firefox()
 
+driver.get("https://steamcommunity.com/market/listings/730/Snakebite%20Case")
 
 try:
-    search = driver.find_element_by_class_name("menu-toggle")
-    outer = search.get_attribute("innerHTML")
-    print(outer)
+    time.sleep(1)
+    search_name = driver.find_element_by_id("largeiteminfo_item_name")
+    search_price = driver.find_element_by_class_name('market_commodity_order_summary')
+    outer_name = search_name.get_attribute("innerHTML")
+    price_name = search_price.get_attribute("textContent")
+    print(outer_name, price_name)
+
+
 finally:
     driver.quit()
-
-
-
-# <span class="market_commodity_orders_header_promote">$1.69</span>
